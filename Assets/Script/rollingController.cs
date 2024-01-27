76a78107c1ef30a46f888e4c208c1f795050aecb
Marsshow <19550137437@163.com>
2024-01-27 20:55:55 +0800

    Version_6.0

--- Assets/Script/rollingController.cs
+++ Assets/Script/rollingController.cs
@@ -8,32 +8,36 @@ public class rollingController : MonoBehaviour
 {
     public Rolling rolling;
 
-    private bool textLock;
-
     // Update is called once per frame
     void Update()
     {
         //internet interface
-        
-        var req = WebRequest.Create("http://192.168.6.155:8008/") as HttpWebRequest;
+        var req = WebRequest.Create("http://"+ReadData(System.IO.Directory.GetCurrentDirectory()+"/Path.txt") +"/") as HttpWebRequest;
 
         var text = "";
         using (var sr = new StreamReader(req.GetResponse().GetResponseStream()))
         {
             text = sr.ReadToEnd();
         }
-        
 
         //Jaw extending
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)||text=="True")
         {
             rolling.jawing = true;
-            textLock = true;
         }
-        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)||(text=="False"&&textLock))
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)||text=="False")
         {
             rolling.jawing = false;
-            textLock = false;
         }
+    }
+
+    public string ReadData(string path)
+    {
+        FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+        StreamReader sr = new StreamReader(fs, System.Text.Encoding.Default);
+        if (null == sr) return "192.168.6.155:8008";
+        string str = sr.ReadToEnd();
+        sr.Close();
+        return str;
     }
 }
